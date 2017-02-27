a9d59435272a4c321784b0a82b4f3365b1fc6b60
mutosunyou <muto@muto.local>
2017-02-27 16:01:44 +0900

    メール配信方法変更

--- helper/sendmail.php
+++ helper/sendmail.php
@@ -18,6 +18,6 @@ $subject = '【回覧通知】'.$js2[0]->title;
 $message = '回覧が来ています。下記URLより、回覧内容をご確認ください。';
 $message.= 'http://192.168.100.209/circular2/disp.php?cid='.$_POST['cid'];
 $message.= '表題：'.$js2[0]->title;
-$headers = 'remote_manager@sunyou.co.jp';
+$headers = 'System<remote_manager@sunyou.co.jp>';
 sendmail($to,'',$subject,$message,$headers);
 
