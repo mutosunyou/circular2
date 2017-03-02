76aca18a055cdc0329727c213ce60e0d830d51b4
mutosunyou <muto@muto.local>
2017-03-02 14:54:12 +0900

    修正

--- helper/automail.php
+++ helper/automail.php
@@ -9,11 +9,12 @@ mb_internal_encoding("UTF-8");
 
 $to='';
 for($i=0;$i<count($result);$i++){
-  $to = mailAddress($);
+  $to = mailAddress($result[$i]['userID']);
   if($i!=(count($)-1)){
     $to.=', ';
   }
 }
+
 $subject = '【回覧通知】未確認の回覧があります';
 $message = '未確認の回覧があります。システムで確認してください。'."\r\n";
 $message.= 'http://192.168.100.209/circular2/list.php'."\r\n";
