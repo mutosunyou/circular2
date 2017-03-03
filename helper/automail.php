7c77e07cbc710c3af28b6928c708d0ede0792e84
mutosunyou <muto@muto.local>
2017-03-03 09:31:37 +0900

    修正

--- helper/automail.php
+++ helper/automail.php
@@ -7,12 +7,12 @@ $result = selectData('circular2', $sql);
 
 $to='';
 for($i=0;$i<count($result);$i++){
-  $to .= mailFromUserID($js3[$i]->num);
+  $to .= mailFromUserID($result[$i]['userID']);
   if($i!=(count($)-1)){
     $to.=', ';
   }
 }
-/*
+
 $subject = '【回覧通知】未確認の回覧があります';
 $message = '未確認の回覧があります。システムで確認してください。'."\r\n";
 $message.= 'http://192.168.100.209/circular2/list.php'."\r\n";
@@ -25,4 +25,4 @@ echo $headers;
 
 sendmail(str_replace('\'','’',$to),'',str_replace('\'','’',$subject),str_replace('\'','’',$message),$headers);
 
- */
+ 
