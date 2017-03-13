9a9082b5684f6bf2384cebe2a56a46a2a1f2ed89
mutosunyou <muto@muto.local>
2017-03-13 10:42:10 +0900

    修理中

--- helper/lister.php
+++ helper/lister.php
@@ -80,9 +80,9 @@ $body .= '</div>';
 $pname = array(
   " "=>"action".' style="text-align:left;width:50px;"',
   "表題"=>"title".' style="text-align:left;"',
-  "状態"=>"status".' style="text-align:left;width:100px;"');
+  "状態"=>"status".' style="text-align:left;width:110px;"');
 if($_POST['own']==0){
-  $pname["作成者"]="ownerID".' style="text-align:left;width:110px;"';
+  $pname["作成者"]="ownerID".' style="text-align:left;width:100px;"';
 }
 $pname["回覧開始日"]="submitDate".' style="text-align:left;width:150px;"';
 
