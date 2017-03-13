528d7953ec9b4edb6ef17e472f8faf74e1741d0f
mutosunyou <muto@muto.local>
2017-03-13 14:04:29 +0900

    修理中

--- uploadifive.php
+++ uploadifive.php
@@ -8,9 +8,9 @@ $fileTypes = array('jpg', 'jpeg', 'gif', 'png', 'pdf','key','xlsx'); // Allowed 
 
 setlocale(LC_ALL, 'ja_JP.UTF-8');
 $fileParts = pathinfo($_FILES['Filedata']['name']);
-$tempFile  = $_FILES['Filedata']['tmp_name'];
-$fname = mb_convert_encoding($fileParts['filename'],"SJIS").'_'.time().'.'.$fileParts['extension'];
-$targetFile = $uploadDir .$fname;
+$tempFile  = mb_convert_encoding($_FILES['Filedata']['tmp_name'],"SJIS","auto");
+$fname = mb_convert_encoding($fileParts['filename'],"SJIS","auto").'_'.time().'.'.$fileParts['extension'];
+$targetFile = $uploadDir.$fname;
 move_uploaded_file($tempFile, $targetFile);
 echo $targetFile;
 ?>
