295538853680f37b9f16f6d5f6b54a7f013596a5
mutosunyou <muto@muto.local>
2017-03-14 15:03:11 +0900

    確認ボタンの位置変更

--- disp.php
+++ disp.php
@@ -48,12 +48,24 @@ $body.='<a class="navbar-brand" href="/php/menu" tabindex="-1"><img alt="Brand" 
 $body.='</div>';
 $body.='<div class="collapse navbar-collapse" id="nav-menu-1">';
 
+$author=0;
+$sql='select * from author';
+$rst=selectData(DB_NAME,$sql);
+for($i=0;$i<count($rst);$i++){
+  if($rst[$i]['userID']==$_SESSION['loginid']){
+    $author=1;
+  }
+}
+
 //左側
 $body.='<ul class="nav navbar-nav">';
 $body.='<li id="listrun" class="bankmenu"><a tabindex="-1">回覧板</a></li>';
 $body.='<li id="list" class="applymenu"><a href="index.php" tabindex="-1">新規作成</a></li>';
 $body.='<li id="input" class="applymenu"><a href="list.php" tabindex="-1">回覧リスト</a></li>';
 $body.='<li id="display" class="active applymenu"><a href="#" tabindex="-1">回覧内容確認</a></li>';
+if($author==1){
+  $body.='<li id="input" class="applymenu"><a href="force.php" tabindex="-1">強制既読</a></li>';
+}
 $body.='</ul>';
 
 //右側
